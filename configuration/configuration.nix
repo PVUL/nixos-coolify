699f63b8ff0a35ec426af48f6c661062eaec7262
PVUL <paul.a.yun@gmail.com>
2024-12-06 18:03:09 +0800

    fix typo

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -42,7 +42,7 @@
     nixos = {
       isNormalUser = true;
       extraGroups = [ "wheel" "docker" ];  # Grants sudo and Docker access.
-      openssh.authorizedKeys.keyFiles = [ 
+      openssh.authorizedKeys.keys = [ 
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDJZoNId6npxlyODyr7/8QJfG5WkpSS6imi8YkcXiHtdymL4RF6wLBvu7mTvDxta/xpzQ1GazUCCsNPtQ7/uepbrPtCcroMeW9WvJcDwRKrh2ejsbofWoQFpyjHNodA7Cz4PFHQSKQijgRrTuHxGkP0uFNkXX5++ucu0Q8uKHn65UGcyYKgIunanY1jp+RzaZ1S6w8W/nI/vM8rCK3ONZBC4bIrVRJWxLIku/1bvPXkD29LN86G184eYI+GzsG7SH79mZ8hoMdgZrGb1ILbcEf/s8Lk+LEsYQiC0OmocUXRaqth/Qf8+SyayA/sxjITf4SfYe1Otv0YtkeERzL3rD3XrTvJa2qiI8g1lFWZ4sBtb/k+FfJJvjkYp5L9+H6HSGF9msHGAQLK9FZqhEN5hTTsHgQtLS4s6RupzOWkWRqx5KmDB1G8/M+YsjD5BuUurSY0luO8I+LJcFGn4HmNTGC4hoAz66uXdosT+wg/ahr5FlFWM7i9oyr7LfoxuFJROdopH1TRRKfSPfOws775+8GODTEgbHYNChNU/HEgqxAaoFPnTkoQ3G9oWp1Ffj7nOIcgaMjTDaf7EIm5faq3RsPfGfcUv1fE4y/nhed0/dSIltW/OLZhZd3vWACe38lmlBYJfHh32YOT/tP0jOhXUtpzArx6M7/M3Apy8esGbL/pxw==" 
       ];
     };
